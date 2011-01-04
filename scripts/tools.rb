2a84e21a8ba2c89ef67e21e3904051b0c937d608
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-01-04 18:29:31 +0100

    update the scripts to use the new wrappers in types_ruby and corridor_planner

--- scripts/tools.rb
+++ scripts/tools.rb
@@ -1,28 +1,4 @@
-require 'typelib'
-
-def traverse(plan, path)
-    last_corridor, last_in_side = path.last
-    last_out_side =
-        if last_in_side == :BACK_SIDE
-            :FRONT_SIDE
-        else :BACK_SIDE
-        end
-
-    next_step = plan.connections.find do |conn|
-        conn.from_idx == last_corridor && conn.from_side == last_out_side &&
-            !path.include?([conn.to_idx, conn.to_side])
-    end
-
-    if !next_step
-        raise "something's wrong: path is blocked"
-    end
-    path << [next_step.to_idx, next_step.to_side]
-    if next_step.to_idx == plan.end_corridor
-        return
-    else
-        traverse(plan, path)
-    end
-end
+require 'corridor_planner/plan'
 
 def output_path(io, plan, path)
     io.puts "X Y Z"
